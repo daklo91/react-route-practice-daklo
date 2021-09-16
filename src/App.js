69ee35dd119faa00bb08190896580d7ecf70196a
daklo91 <coffeedog91@gmail.com>
2021-09-16 13:30:11 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch } from "react-router";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetups">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
