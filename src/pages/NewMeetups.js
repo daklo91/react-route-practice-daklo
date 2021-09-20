2b7e54e080910eef885a711ca6e03ca496cef7c3
daklo91 <coffeedog91@gmail.com>
2021-09-20 19:23:42 +0200

    Add fetch post

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,29 @@
+import { useHistory } from "react-router";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-  return <div>This is the NewMeetups page</div>;
+  const history = useHistory();
+
+  function postNewMeetup(meetupData) {
+    fetch(
+      "https://react-tutorial-course-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <div>
+      <h1>Create a new meetup</h1>
+      <NewMeetupForm postNewMeetup={postNewMeetup} />
+    </div>
+  );
 }
 
 export default NewMeetupsPage;
